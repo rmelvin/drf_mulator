4c68a068cc7b817993d4ecfd32d148417ced5543
Reggie Melvin <rmelvin@mediatemple.net>
2016-05-03 09:52:49 -0700

    add setup.py
    add .gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='drf-mulator',
+      version='0.1',
+      description='Django Rest Framework API Emulator',
+      author='Reggie Melvin',
+      author_email='rmelvin09@hotmail.com',
+      packages=['mulator'],
+     )
